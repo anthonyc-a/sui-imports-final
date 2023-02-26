8c775a754fda47752241d4ed9cec65ed954409b9
Anthony <anthonyc.animba@gmail.com>
2023-02-26 00:16:52 +0000

    fix

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,15 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { datas } from '../test'
 
 type Data = {
-  name: string,
-  age: number
+  name: number
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json(datas)
+  res.status(200).json({ name: 1 })
 }
--- pages/test.ts
+++
@@ -1,1 +0,0 @@
-export const datas = {name:"ant", age: 22}
