3726236797db8b1695e9371c4aba28e5356f0e29
Anthony <anthonyc.animba@gmail.com>
2023-02-27 21:20:33 +0000

    fix: clean

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <title>SUI Wine Imports</title>
+      <meta name="description" content="SUI Wine Imports Official Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link rel="icon" href="/lgo-c.ico" />
+        <link
+      href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600&display=swap"
+      rel="stylesheet"
+    />
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
