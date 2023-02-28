7f41476b80187ad3e2d77946fd28576f0e4173e0
Anthony <anthonyc.animba@gmail.com>
2023-02-28 00:07:24 +0000

    ...

--- components/common/Scrollbar/Scrollbar.tsx
+++ components/common/Scrollbar/Scrollbar.tsx
@@ -3,33 +3,33 @@ import React, { useEffect, useState } from "react"
 import styles from './Scrollbar.module.css';
 
 const Scrollbar = () => {
-  const [scrollTop, setScrollTop] = useState(0)
+    const [scrollTop, setScrollTop] = useState(0)
 
-  const onScroll = () => {
-    const winScroll = document.documentElement.scrollTop
-    const height =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight
+    const onScroll = () => {
+        const winScroll = document.documentElement.scrollTop
+        const height =
+            document.documentElement.scrollHeight -
+            document.documentElement.clientHeight
 
-    const scrolled = (winScroll / height) * 100
+        const scrolled = (winScroll / height) * 100
 
-    setScrollTop(scrolled)
-  }
+        setScrollTop(scrolled)
+    }
 
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll)
+    useEffect(() => {
+        window.addEventListener("scroll", onScroll)
 
-    return () => window.removeEventListener("scroll", onscroll)
-  }, [])
+        return () => window.removeEventListener("scroll", onScroll)
+    }, [])
 
-  return (
-    <div className={styles.scrollbar}>
-      <div
-        className={styles.scrollbarInner}
-        style={{ width: `${scrollTop}%` }}
-      ></div>
-    </div>
-  )
+    return (
+        <div className={styles.scrollbar}>
+            <div
+                className={styles.scrollbarInner}
+                style={{ width: `${scrollTop}%` }}
+            ></div>
+        </div>
+    )
 }
 
 export default Scrollbar
